17c323720e2563ef4c0a75efdf11a36edf10124e
masashi-sutou <sutou.masasi@gmail.com>
2016-03-11 04:10:29 +0900

    third_demo

--- BlendingCulturesExample/Classes/Models/DataSource.swift
+++ BlendingCulturesExample/Classes/Models/DataSource.swift
@@ -1,47 +1,50 @@
-////
-////  DataSource.swift
-////  BlendingCulturesExample
-////
-////  Created by Suguru Kishimoto on 2016/03/03.
-////
-////
 //
-//import UIKit
+//  DataSource.swift
+//  BlendingCulturesExample
 //
-//class DataSource: NSObject, SourceType {
-//    var dataObject: DataType
-//    var conditionForAdding: Bool {
-//        return false
-//    }
-//    
-//    init<A: DataType>(dataObject: A) {
-//        self.dataObject = dataObject
-//    }
+//  Created by Suguru Kishimoto on 2016/03/03.
 //
-//    func addItemTo(tableView: UITableView) {
-//        if conditionForAdding {
-//            dataObject = dataObject.addNewItemAtIndex(0)
-//            insertTopRowIn(tableView)
-//        }
-//    }
-//    
-//    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        return dataObject.numberOfItems
-//    }
-//    
-//    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-//        fatalError("This method must be overridden")
-//    }
-//    
-//    func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-//        if editingStyle == .Delete {
-//            dataObject = dataObject.deleteItemAtIndex(indexPath.row)
-//            deleteRowAtIndexPath(indexPath, from: tableView)
-//        }
-//    }
-//    
-//    func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
-//        dataObject = dataObject.moveItem(fromIndexPath.row, toIndex: toIndexPath.row)
-//    }
 //
-//}
+
+import UIKit
+
+class DataSource: NSObject, UITableViewDataSource, SourceType {
+    
+    var dataObject: DataType = Hand()
+    
+    func addItemTo(tableView: UITableView) {
+        if dataObject.numberOfItems < 5 {
+            dataObject = dataObject.addNewItemAtIndex(0)
+            insertTopRowIn(tableView)
+        }
+    }
+
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dataObject.numberOfItems
+    }
+
+    func tableView(tableView: UITableView,
+                   cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+            guard let cell = tableView
+                             .dequeueReusableCellWithIdentifier("cardCell",
+                                                                forIndexPath: indexPath)
+                             as? CardCell, hand = dataObject as? Hand else {
+                        
+                    fatalError("Could not create CardCell or Hand instance")
+            }
+            
+            cell.fillWith(hand[indexPath.row])
+            return cell
+    }
+
+    func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if editingStyle == .Delete {
+            dataObject = dataObject.deleteItemAtIndex(indexPath.row)
+            deleteRowAtIndexPath(indexPath, from: tableView)
+        }
+    }
+    
+    func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
+        dataObject = dataObject.moveItem(fromIndexPath.row, toIndex: toIndexPath.row)
+    }
+}
--- BlendingCulturesExample/Classes/Models/SourceType.swift
+++ BlendingCulturesExample/Classes/Models/SourceType.swift
@@ -1,26 +1,27 @@
-////
-////  SourceType.swift
-////  BlendingCulturesExample
-////
-////  Created by Suguru Kishimoto on 2016/03/03.
-////
-////
 //
-//import UIKit
+//  SourceType.swift
+//  BlendingCulturesExample
 //
-//protocol SourceType: UITableViewDataSource {
-//    var dataObject: DataType { get set }
-//    var conditionForAdding: Bool { get }
-//    func insertTopRowIn(tableView: UITableView)
-//    func deleteRowAtIndexPath(indexPath: NSIndexPath, from tableView: UITableView)
-//}
+//  Created by Suguru Kishimoto on 2016/03/03.
 //
-//extension SourceType {
-//    func insertTopRowIn(tableView: UITableView) {
-//        tableView.insertRowsAtIndexPaths([NSIndexPath(forRow: 0, inSection: 0)], withRowAnimation: .Fade)
-//    }
-//    
-//    func deleteRowAtIndexPath(indexPath: NSIndexPath, from tableView: UITableView) {
-//        tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-//    }
-//}
+//
+
+import UIKit
+
+protocol SourceType: UITableViewDataSource {
+    
+    var dataObject: DataType { get set }
+    
+    func insertTopRowIn(tableView: UITableView)
+    func deleteRowAtIndexPath(indexPath: NSIndexPath, from tableView: UITableView)
+}
+
+extension SourceType {
+    func insertTopRowIn(tableView: UITableView) {
+        tableView.insertRowsAtIndexPaths([NSIndexPath(forRow: 0, inSection: 0)], withRowAnimation: .Fade)
+    }
+    
+    func deleteRowAtIndexPath(indexPath: NSIndexPath, from tableView: UITableView) {
+        tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+    }
+}
